7005876e720f0ef52a8ff83e150afbfb21a82a8a
ghylc <ppqlover@gmail.com>
2025-05-13 07:20:13 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -2,16 +2,11 @@ from playwright.sync_api import sync_playwright
 import os
 import requests
 
-from apscheduler.schedulers.blocking import BlockingScheduler
-
 def refresh_url():
     url = 'https://web.css63.ggff.net/sub'
     response = requests.get(url)
-    print(response.text)  # 打印URL的响应内容
-
-scheduler = BlockingScheduler()
-scheduler.add_job(refresh_url, 'interval', minutes=1)
-scheduler.start()
+    print(response.text)
+refresh_url()
 
 def send_telegram_message(message):
     bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
